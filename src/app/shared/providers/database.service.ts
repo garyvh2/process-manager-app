7b2b4b9e385bfd357b3c0a7a645d89763ba43c7a
Gary Valverde Hampton <garyhampton.12@gmail.com>
2018-07-03 21:14:29 -0600

    feat(providers): Add inital DB Service

---
+++ src/app/shared/providers/database.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AppSettings } from '../../app.settings';
+
+@Injectable()
+export class ConnService {
+
+  constructor (private HTTP: HttpClient) {
+
+  }
+
+  getAll (type): Observable<any> {
+    return this.HTTP.get(AppSettings.API_LOCATION + type);
+  }
+
+  find (type) {
+    return this.HTTP.get(AppSettings.API_LOCATION + type);
+
+  }
+
+  create () {
+
+  }
+
+  update () {
+
+  }
+
+  delete () {
+
+  }
+
+}
