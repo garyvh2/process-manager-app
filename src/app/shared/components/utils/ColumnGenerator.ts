e2feab22cf00764d030a7dae7c3ba1f13c0c5e4c
Gary Valverde Hampton <garyhampton.12@gmail.com>
2018-07-17 01:51:43 -0600

    feat(utils): Column Generator

--- src/app/shared/components/utils/ColumnGenerator.ts
+++ src/app/shared/components/utils/ColumnGenerator.ts
@@ -1,8 +1,28 @@
 import { BaseEntity } from "../../../models/interface/BaseEntity";
+import { chain, keys } from "underscore";
+import { CustomRenderComponent } from "./CustomRender";
+import { Humanizer } from "./humanize/Humanizer";
+import { HumanizeFactory } from "./humanize/HumanizeFactory";
 
 export class ColumnGenerator {
     constructor () {}
+
     public static generate(object: BaseEntity): Object {
-        return {};
+        const PROPERTIES = keys(object);       
+        const TitleHumanizer = (new HumanizeFactory()).create("String");
+
+        let settings = chain(PROPERTIES)
+            .map(EL => {
+                return {
+                    id: EL,
+                    title: TitleHumanizer.process(EL),
+                    type: "custom",
+                    renderComponent: CustomRenderComponent
+                }
+            })
+            .indexBy(EL => EL.id)
+            .value();
+
+        return settings;
     }
 }
