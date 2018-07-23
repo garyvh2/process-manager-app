fe14afe7cf3257ce62a9b867fe7e80577c1a56bf
Jose Solano <dsolano@builderhomesite.com>
2018-07-22 23:34:47 -0600

    Finishing Login
    Set current user in local storage.
    Validate form.
    Fixing issue when error.

--- src/app/modules/Login/pages/Login/login.component.ts
+++ src/app/modules/Login/pages/Login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import { ConnService } from '../../../../shared/providers/database.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ConnService: ConnService) { }
 
   ngOnInit() {
+  }
+
+  loginFunc () {
+    let userName = $('#userName').val();
+    let password = $('#password').val();
+    if(!this.isValid(userName,password)){
+      alert("Complete los campos requeridos");
+      return;
+    }
+    this.sendRequest(userName, password);
+    console.log("Jose");
+  }
+
+  isValid(userName, password) {
+    return userName !== '' && password !== '';
+  }
+
+  sendRequest(userName, password){
+    this.ConnService.create<User>({
+      endpoint: "users/authenticateUser",
+      obj :{
+        userEmail : userName,
+        userPassword: password
+      }
+    }).subscribe(
+      data => {
+        this.saveInLocalStorage(data);
+      },
+      error => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  saveInLocalStorage(obj) {
+    localStorage.setItem('UserLogged', JSON.stringify(obj));
   }
 
 }
