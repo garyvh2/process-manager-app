69d96313897fcebe46108006c5e8ffa14228334e
Gary Valverde <gvalverde@ex2outcoding.local>
2018-08-11 21:24:13 -0600

    feat(kuker): Track States

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { AngularEmitter } from 'kuker-emitters';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+AngularEmitter();
 
 if (environment.production) {
   enableProdMode();
